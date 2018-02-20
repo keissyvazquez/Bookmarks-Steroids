9b27971fe39f83d6f09a1ce18fea87e32fd241eb
Keissy <keissy@lifecellskincare.com>
2018-02-20 10:32:12 -0500

    Changes for authentification.

--- routes/loginroutes.js
+++ routes/loginroutes.js
@@ -42,25 +42,29 @@ exports.register = function(req,res){
 }
 
 exports.login = function(req,res){
+  console.log("Logging in");
   var email= req.body.email;
   var password = req.body.password;
   connection.query('SELECT * FROM users WHERE email = ?',[email], function (error, results, fields) {
   if (error) {
-    // console.log("error ocurred",error);
+    console.log("error ocurred",error);
     res.send({
       "code":400,
       "failed":"error ocurred"
     })
   }else{
-    // console.log('The solution is: ', results);
+    console.log('The solution is: ', results);
     if(results.length >0){
-      if([0].password == password){
+      if(results[0].password == password){
+        console.log("Password good");
         res.send({
           "code":200,
-          "success":"login sucessfull"
+          "success":"login sucessfull",
+          "location": "profile.html"
             });
       }
       else{
+        console.log("Password bad");
         res.send({
           "code":204,
           "success":"Email and password does not match"
