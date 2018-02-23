8fba17257dfaa453142a26e3eb0cd1c499ca8c40
Keissy <keissy@lifecellskincare.com>
2018-02-23 14:05:52 -0500

    system logs in to profile screen, working on local storage

--- public/assets/javascript/script.js
+++ public/assets/javascript/script.js
@@ -1,19 +1,52 @@
-  var button = document.getElementById("login"); 
 
-  button.addEventListener("click", function(){ 
-    var email = document.getElementById("email-input").value; 
-    var password = document.getElementById("password-input").value;
-    console.log(email);
-    console.log(password);
-     $.post( "login.html", { email: "John@gmail.com", password: "123456" })
-	    .done(function( data ) {
-	     alert( "Data Loaded: " + data );
+  $(document).ready(function(){
+    $('#login_form').submit(function(){
+      var emailB = $('#email-input').val();
+      var passwordB = $('#password-input').val();
+      $.ajax({
+        type: 'POST',
+        url: 'http://localhost:5000/login',
+        // dataType: json,
+        data: {
+          email: emailB,
+          password: passwordB,
+        },
+        success: function(result){
+          console.log("Login successful");
+
+          // TODO: Here you should be saving the email and password
+          document.location.href= 'profile.html';
+        },
+        error: function(jqXHR, status, errorThrown) {
+          console.log(status + " and " + errorThrown);
+        }
+
+      })
+      return false;
     });
 
+    $('#login').click(function(){
+      $('#login_form').submit()
+    });
+  });
 
-  }); 
 
-  $("#signup").on("click", function(){ 
-  	location.href = "/signup.html"
-
-  })
+  //
+  // var settings = {
+  //   "async": true,
+  //   "crossDomain": true,
+  //   "url": "http://localhost:5000/login",
+  //   "method": "POST",
+  //   "headers": {
+  //     "authorization": "Basic anF1aW5vbmVzQHlhaG9vLmNvbTpsaWVz",
+  //     "cache-control": "no-cache",
+  //   },
+  //   "data": {
+  //     // "email": "jquinones@yahoo.com",
+  //     // "password": "cleaning"
+  //   }
+  // }
+  //
+  // $.ajax(settings).done(function (response) {
+  //   console.log(response);
+  // });
