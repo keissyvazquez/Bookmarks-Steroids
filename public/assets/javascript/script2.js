f38b62d1facfb1522a8617f2528a8ed98a122f00
Keissy <keissy@lifecellskincare.com>
2018-02-26 18:53:03 -0500

    desktop profile partly completed

--- public/assets/javascript/script2.js
+++ public/assets/javascript/script2.js
@@ -1,7 +1,35 @@
-document.body.onload = function() {
-  chrome.storage.sync.get("data", function(items) {
-    if (!chrome.runtime.error) {
-      console.log(items);
-    }
-  });
-}
+document.body.onload = function(){
+	var settings = {
+	  "async": true,
+	  "crossDomain": true,
+	  "url": "http://localhost:5000/api/urls/",
+	  "method": "GET",
+	  "headers": {
+	    "Content-Type": "application/json",
+	    "Authorization": "Basic anF1aW5vbmVzQHlhaG9vLmNvbTpjbGVhbmluZw==",
+	    "Cache-Control": "no-cache",
+	  	},
+	  "processData": false,
+	  "data": "{\n\t\"name\": \"Another store\"\n}"
+	}; 
+
+	var bookmark = {
+			"title": title,
+			"description": description,
+			"category": category
+	}; 
+	var categoryID = document.getElementById("category"); 
+	var titleID = document.getElementById("title"); 
+	var descriptionID = document.getElementById("description"); 
+	var column = document.getElementById("column"); 
+	var tableRow = document.getElementById("tableRow"); 
+
+   	$.ajax(settings).done(function (response) {
+   		 
+   		 
+		  console.log(response);
+		});
+
+} 
+	
+	
